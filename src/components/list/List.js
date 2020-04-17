067e2654bc170cc1812b0d8dbe1b8483d74ebcce
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-04-17 11:28:52 -0700

    add webpack.config

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './PortfolioList.css';
+import styles from './List.css';
 import { Link } from 'react-router-dom';
 import { listData } from '../../data/listData';
 
--- src/components/list/ListItem.js
+++ src/components/list/ListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './PortfolioItem.css';
+import styles from './ListItem.css';
 import PropTypes from 'prop-types';
 import { useList } from '../../hooks/list';
 import { Link } from 'react-router-dom';
