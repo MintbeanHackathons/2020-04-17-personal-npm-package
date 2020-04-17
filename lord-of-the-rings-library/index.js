03873f4fcd5061e69d942ff4c928b47f5eccb18d
Fred <frederique.bordeleau@gmail.com>
2020-04-17 15:55:24 -0400

    Added index.js file and exported all functions

---
+++ lord-of-the-rings-library/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  characterCaracteristics: () => {},
+  charactersListInOrder: () => {},
+  createdYourOwnCharacter: () => {},
+  ifCharacterNotFound: () => {},
+  kindOfCharacter: () => {}
+};
