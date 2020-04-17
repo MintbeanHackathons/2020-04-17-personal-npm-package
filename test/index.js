c3caef501d1ee827629238a19034b3eae7c36747
Sam Bernal <sambernaldamasco@gmail.com>
2020-04-17 14:43:13 -0400

    added comments to each method

--- test/index.js
+++ test/index.js
@@ -1,10 +1,24 @@
+//==========================================================================
+// MATRIX UTIlS PACKAGE
+//==========================================================================
+// author: Sam Bernal
+// version: 1.0
+// description: methods to help creation and loop through multidimensional arrays
+//==========================================================================
+
 //calling it matrix since it's for only multidimensional arrays
 //choosing old ES5 function call for scoping
 //has 3 parameters
 //size for X(row)
 //size for Y(column)
 //value, a function that will determine the value of each elemenet
-const matrixUtils = {
+module.exports = {
+	//==========================================================================
+	// CREATEMATRIX METHOD
+	//==========================================================================
+	// PARAMS: (sizeX: number, sizeY: number, value: function)
+	// description: creates a multidimensional array based on the size of X and Y
+	// the value is determined by a function call, otherwise all values will be undefined
 	createMatrix(sizeX, sizeY, value = () => undefined) {
 		const matrix = [];
 		for (let row = 0; row < sizeX; row++) {
@@ -16,7 +30,12 @@ const matrixUtils = {
 		return matrix;
 	}, // end of createMatrix function
 
-	//
+	//==========================================================================
+	// LOOPMATRIX METHOD
+	//==========================================================================
+	// PARAMS: (array: array or object, func: function)
+	// description: validates if 1st param passed is an array or object
+	// loops through the array(or object) and performs a function to manipulate the array
 	loopMatrix(
 		array,
 		func = (element) => {
@@ -48,9 +67,14 @@ const matrixUtils = {
 		}
 	}, // end of loopmatrix function
 
+	//==========================================================================
+	// CLONEMATRIX METHOD
+	//==========================================================================
+	// PARAMS: (array: array or object)
+	// description: javascript doesn't allow cloning multidimensional arrays,
+	// it creates a reference instead this function parses the array as a JSON
+	// then stringify to be able to clone it  without being a reference
 	cloneMatrix(array) {
 		return JSON.parse(JSON.stringify(array));
 	}, //end of clone function
 };
-
-console.log(matrixUtils.createMatrix(5, 5));
