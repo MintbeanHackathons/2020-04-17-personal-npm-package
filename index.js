6c378633bb6209c1958c12caf94e34be67bc73ce
Link Fang <fang0055@algonquinlive.com>
2020-04-17 13:57:53 -0400

    finished the basic version

---
+++ index.js
@@ -0,0 +1,22 @@
+const convertUnits = {
+    weight: {
+        gramsToPounds: (grams)=>{
+            return grams * 0.00220462
+        },
+
+        kilogramsToPounds: (kg)=>{
+            return kg * 2.20462262
+        },
+
+        poundsToKilograms: (pounds)=>{
+            return pounds * 0.45359237
+        },
+
+        poundsToGrams: (pounds)=>{
+            return pounds * 453.59237
+        }
+    }
+}
+
+module.exports = convertUnits
+
