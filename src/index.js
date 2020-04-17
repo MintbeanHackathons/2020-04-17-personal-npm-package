bd2c6c5ee721284fe1a2e85932aa78bc0e3c6c3d
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-04-17 11:46:52 -0700

    add name to package.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
-import './reset.css';
 
 render(
   <App />,
--- webpack.config.js
+++ webpack.config.js
@@ -5,72 +5,72 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 // eslint-disable-next-line
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.[hash].js',
-        publicPath: '/'
-    },
-    devServer: {
-        port: 7890,
-        historyApiFallback: true
-    },
-    plugins: [
-        new HtmlPlugin({ template: './src/index.html' }),
-        new CleanWebpackPlugin(),
-        new Dotenv({
-            systemvars: true
-        }),
-        new CopyPlugin([
-            { from: 'public' },
-        ])
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        cacheDirectory: true
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
-                            modules: true,
-                            importLoaders: 1
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            sourceMap: true,
-                            plugins: [
-                                require('postcss-import')(),
-                                require('autoprefixer')(),
-                                require('postcss-nested')(),
-                                require('postcss-simple-vars')()
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(jpeg|jpg|png|svg)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: { limit: 1000 },
-                },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[hash].js',
+    publicPath: '/'
+  },
+  devServer: {
+    port: 7890,
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin(),
+    new Dotenv({
+      systemvars: true
+    }),
+    new CopyPlugin([
+      { from: 'public' },
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+              modules: true,
+              importLoaders: 1
             }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('postcss-import')(),
+                require('autoprefixer')(),
+                require('postcss-nested')(),
+                require('postcss-simple-vars')()
+              ]
+            }
+          }
         ]
-    }
+      },
+      {
+        test: /\.(jpeg|jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: { limit: 1000 },
+        },
+      }
+    ]
+  }
 };
