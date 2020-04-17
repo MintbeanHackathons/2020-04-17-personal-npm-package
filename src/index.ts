81c856237b764d0788a2e977c99a85952ebec1b2
Max <montrealist@users.noreply.github.com>
2020-04-17 14:11:35 -0400

    added jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 /**
+ * Given an array of integers, count the number of elements X such that (X + 1) is also in the array.
+ *
+ * Example:
+ * Input: [1,2,3]
+ * Output: 2
+ * Explanation: 1 and 2 are counted because (1 + 1) = 2 and (2 + 1 ) = 3 are in the array.
+ *
  * @param {number[]} arr
  * @return {number}
  */
