6862b69ef87f593926db2cf5a954bc166a7b61b9
aaranda82 <alexraranda@gmail.com>
2020-04-17 11:35:45 -0700

    udated index.js with addArrays method

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ module.exports.makeArrayLowercase = stringArray => {
   const lowered = stringArray.map(item=>item.toLowerCase())  
   return lowered
 }
+
+module.exports.addArrays = (arr1, arr2) => {
+  if(arr1.length < arr2.length) {
+    const len = arr1.length
+  } else {
+    const len = arr2.length
+  }
+  const sumOfArrays = []
+  for( let x = 0; x < len; x++) {
+    if(typeof arr1[x] !== "number" || typeof arr2[x] !== "number" ) {
+      sumOfArrays.push(NaN)
+    } else { 
+      sumOfArrays.push(arr1[x] + arr2[x])
+    }
+  }
+}
