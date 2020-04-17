4d25683f7d1160ddc0f1cc815fb01292e609cf65
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-04-17 11:40:03 -0700

    troubleshoot build

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from '../components/header/Header';
 import ListItem from '../components/list/ListItem';
 import List from '../components/list/List';
 import './App.css';
@@ -8,7 +7,6 @@ import './App.css';
 export default function App() {
   return (
     <Router>
-      <Header />
       <Route exact path='/' component={List}/>
       <Switch>
         <Route path='/:title' component={ListItem} />
