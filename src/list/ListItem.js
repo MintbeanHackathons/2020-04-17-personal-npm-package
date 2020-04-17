ab122f291cd5cc5d7ecea382f0621db3d3526514
Lisa Carpenter <carpenter.lisa@gmail.com>
2020-04-17 11:22:10 -0700

    add instructions to readme for utilizing components

--- src/list/ListItem.js
+++ src/list/ListItem.js
@@ -4,15 +4,12 @@ import PropTypes from 'prop-types';
 import { useList } from '../../hooks/list';
 import { Link } from 'react-router-dom';
 
-const PortfolioItem = ({ match }) => {
-  const { title, image, description, script } = usePortfolio(match.params.title);
+const ListItem = ({ match }) => {
+  const { title, image, description } = useList(match.params.title);
 
   return (
-    <div className={styles.PortfolioItem}>
+    <div className={styles.ListItem}>
       <h2 className={styles.title}>{title}</h2>
-      <div className={styles.links}>
-        <a href={script} target='_blank' rel='noopener noreferrer' >Code</a>
-      </div>
       <img src={image} alt={title} />
       <p className={styles.description}>{description}</p>
       <Link className={styles.Link} to={'/'}>Back</Link>
@@ -20,7 +17,7 @@ const PortfolioItem = ({ match }) => {
   );
 };
 
-PortfolioItem.propTypes = { 
+ListItem.propTypes = { 
   match: PropTypes.shape({
     params: PropTypes.shape({
       title: PropTypes.string.isRequired
@@ -28,4 +25,4 @@ PortfolioItem.propTypes = {
   }).isRequired
 };
 
-export default PortfolioItem;
+export default ListItem;
