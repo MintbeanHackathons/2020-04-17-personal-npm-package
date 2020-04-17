59f19c99df3cae0a3e46a038938da30fdf7b23ed
AndrewZaw <andrew502502@gmail.com>
2020-04-17 14:15:38 -0400

    modified index.js

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const to = require('./to');
 const hashPassword = require('./hashPassword');
 const validatePassword = require('./validatePassword');
 
-module.exports = connectToMongoDB;
-module.exports = to;
-module.exports = hashPassword;
-module.exports = validatePassword;
+module.exports = {
+  connectToMongoDB,
+  to,
+  hashPassword,
+  validatePassword,
+};
--- to.js
+++ to.js
@@ -3,19 +3,6 @@ The to method is intended to provide a better syntax for try/catch blocks.
 It makes it so try/catch blocks are not necessary, especially to avoid nested try/catch blocks.
 @param {Promise} promise - The promise that you'd like to use. 
 @return {Promise} - If successful, returns array with null as error. Otherwise, returns array with err as error. This can be accessed with the first element of the array.
-
-Example: 
-
-const [databaseError] = await to(accessDatabase());
-if (databaseError){
-  // error handling
-}
-
-const [serverError] = await to(accessServer());
-if (serverError){
-  // more error handling
-}
-
 */
 
 const to = (promise) => {
