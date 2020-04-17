5c282fa9cedd6092298351a6a244ca06e849712b
Link Fang <fang0055@algonquinlive.com>
2020-04-17 14:53:05 -0400

    added some length conversion

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 module.exports = {
     weight: {
-        gramsToPounds: (grams)=>{
+        gramsToPounds: (grams) => {
             return grams * 0.00220462
         },
 
-        kilogramsToPounds: (kg)=>{
+        kilogramsToPounds: (kg) => {
             return kg * 2.20462262
         },
 
-        poundsToKilograms: (pounds)=>{
+        poundsToKilograms: (pounds) => {
             return pounds * 0.45359237
         },
 
-        poundsToGrams: (pounds)=>{
+        poundsToGrams: (pounds) => {
             return pounds * 453.59237
         }
     },
 
     length: {
-        
+        cmToInch: (cm) => {
+            return cm * 0.39370079
+        },
+
+        cmToFoot: (cm) => {
+            return cm * 0.0328084
+        },
+
+        footToInch: (foot) => {
+            return foot * 12
+        },
+
+        inchToFoot: (inch) => {
+            return inch * 0.08333333
+        }
     }
 }
 
