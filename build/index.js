18def02e1feabad7644447f6c32ed86e851f5db4
claire froelich <claire.froelich@gmail.com>
2020-04-17 14:58:12 -0400

    init

--- build/index.js
+++ build/index.js
@@ -84,24 +84,120 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }(); // Inspired by Kristin Baumann's countdown component, converted to hooks
+// https://medium.com/@kristin_baumann/react-countdown-6455838b6faf
+
 var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
+var PROGRESS_BAR_RADIUS = 161; // px
 
 var Countdown = function Countdown(_ref) {
-  _objectDestructuringEmpty(_ref);
+  var text = _ref.text,
+      targetDate = _ref.targetDate,
+      tMinusNDays = _ref.tMinusNDays;
+
+  var _useState = (0, _react.useState)(0),
+      _useState2 = _slicedToArray(_useState, 2),
+      timeRemaining = _useState2[0],
+      setTimeRemaining = _useState2[1];
+
+  var _useState3 = (0, _react.useState)(0),
+      _useState4 = _slicedToArray(_useState3, 2),
+      progress = _useState4[0],
+      setProgress = _useState4[1];
+
+  (0, _react.useEffect)(function () {
+    var timeLeft = calculateCountdown(targetDate);
+    timeLeft && setTimeRemaining(timeLeft);
+    setProgress(Math.round(360 * (tMinusNDays - timeLeft.days) / tMinusNDays));
+  }, [targetDate]);
+
+  var calculateCountdown = function calculateCountdown(endDate) {
+    var diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+
+    // clear countdown when date is reached
+    if (diff <= 0) return false;
+
+    var timeLeft = {
+      days: 0
+    };
+
+    if (diff >= 86400) {
+      // 24 * 60 * 60
+      timeLeft.days = Math.floor(diff / 86400);
+      diff -= timeLeft.days * 86400;
+    }
+    return timeLeft;
+  };
+
+  var addLeadingZeros = function addLeadingZeros(value) {
+    value = String(value);
+    while (value.length < 2) {
+      value = '0' + value;
+    }
+    return value;
+  };
+
+  // Math from Matt's answer:
+  // https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle/24569190#24569190
+
+  // for calculating progress segment of SVG
+  var polarToCartesian = function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + radius * Math.cos(angleInRadians),
+      y: centerY + radius * Math.sin(angleInRadians)
+    };
+  };
+
+  var describeArc = function describeArc(x, y, radius, startAngle, endAngle) {
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+    var d = ["M", start.x, start.y, "A", radius, radius, 0, arcSweep, 0, end.x, end.y].join(" ");
+
+    return d;
+  };
 
   return _react2.default.createElement(
     'div',
-    null,
+    { className: 'countdown' },
     _react2.default.createElement(
-      'p',
+      'h4',
       null,
-      'test'
+      text
+    ),
+    _react2.default.createElement(
+      'div',
+      { className: 'countdown-date-group' },
+      _react2.default.createElement(
+        'div',
+        { className: 'countdown-date-item' },
+        _react2.default.createElement(
+          'strong',
+          null,
+          addLeadingZeros(timeRemaining.days ? timeRemaining.days : ' . ')
+        ),
+        _react2.default.createElement(
+          'span',
+          null,
+          'Jour' + (timeRemaining.days !== 1 && 's')
+        )
+      )
+    ),
+    _react2.default.createElement(
+      'svg',
+      { className: 'countdown-circle', viewBox: '-8 -8 338 338', height: '100', width: '100' },
+      _react2.default.createElement('circle', { className: 'countdown-circle-back', cx: PROGRESS_BAR_RADIUS, cy: PROGRESS_BAR_RADIUS, r: PROGRESS_BAR_RADIUS, fill: 'none' }),
+      _react2.default.createElement('path', { className: 'countdown-circle-progress', d: describeArc(PROGRESS_BAR_RADIUS, PROGRESS_BAR_RADIUS, PROGRESS_BAR_RADIUS, 0, progress), 'stroke-width': '8', fill: 'none' })
     )
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,88 @@
-import React from 'react';
+// Inspired by Kristin Baumann's countdown component, converted to hooks
+// https://medium.com/@kristin_baumann/react-countdown-6455838b6faf
 
-const Countdown = ({}) => (
-  <div>
-    <p>test</p>
-  </div>
-);
+import React, { useState, useEffect } from 'react'
+
+const PROGRESS_BAR_RADIUS = 161 // px
+
+const Countdown = ({ text, targetDate, tMinusNDays }) => {
+  const [timeRemaining, setTimeRemaining] = useState(0)
+  const [progress, setProgress] = useState(0)
+
+  useEffect(() => {
+    const timeLeft = calculateCountdown(targetDate);
+    timeLeft && setTimeRemaining(timeLeft)
+    setProgress(Math.round((360 * (tMinusNDays - timeLeft.days))/tMinusNDays))
+  }, [targetDate])
+
+  const calculateCountdown = (endDate) => {
+    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000
+
+    // clear countdown when date is reached
+    if (diff <= 0) return false;
+
+    const timeLeft = {
+      days: 0,
+    }
+
+    if (diff >= 86400) { // 24 * 60 * 60
+      timeLeft.days = Math.floor(diff / 86400);
+      diff -= timeLeft.days * 86400;
+    }
+    return timeLeft;
+  }
+
+  const addLeadingZeros = (value) => {
+    value = String(value);
+    while (value.length < 2) {
+      value = '0' + value;
+    }
+    return value;
+  }
+
+  // Math from Matt's answer:
+  // https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle/24569190#24569190
+
+  // for calculating progress segment of SVG
+  const polarToCartesian = (centerX, centerY, radius, angleInDegrees) => {
+    const angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
+  }
+
+  const describeArc = (x, y, radius, startAngle, endAngle) => {
+
+      const start = polarToCartesian(x, y, radius, endAngle);
+      const end = polarToCartesian(x, y, radius, startAngle);
+
+      const arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+
+      const d = [
+          "M", start.x, start.y,
+          "A", radius, radius, 0, arcSweep, 0, end.x, end.y,
+      ].join(" ");
+
+      return d;
+  }
+
+  return (
+    <div className="countdown">
+      <h4>{text}</h4>
+      <div className="countdown-date-group">
+        <div className="countdown-date-item">
+          <strong>{addLeadingZeros(timeRemaining.days ? timeRemaining.days : ' . ' )}</strong>
+          <span>{`Jour${timeRemaining.days !== 1 && 's'}`}</span>
+        </div>
+      </div>
+      <svg className="countdown-circle" viewBox="-8 -8 338 338" height="100" width="100">
+        <circle className="countdown-circle-back" cx={PROGRESS_BAR_RADIUS} cy={PROGRESS_BAR_RADIUS} r={PROGRESS_BAR_RADIUS}  fill="none" />
+        <path className="countdown-circle-progress" d={describeArc(PROGRESS_BAR_RADIUS, PROGRESS_BAR_RADIUS, PROGRESS_BAR_RADIUS, 0, progress)} stroke-width="8" fill="none" />
+      </svg>
+    </div>
+  )
+}
 
 export default Countdown;
