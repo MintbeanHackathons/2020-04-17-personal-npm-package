01944d586ddd1a6ef9523ab028b67f6db3aefd2e
AndrewZaw <andrew502502@gmail.com>
2020-04-17 14:33:56 -0400

    updated hashPassword

--- hashPassword.js
+++ hashPassword.js
@@ -5,17 +5,10 @@ The hashPassword method hashes a password using bcrypt.
 @return {Promise} - Returns the hashed password if successful, otherwise returns the error
 */
 const bcrypt = require('bcryptjs');
-const to = require('./to');
 
 const hashPassword = async (password, saltLength = 10) => {
-  const [saltError, salt] = to(await bcrypt.genSalt(saltLength));
-  if (saltError) {
-    return saltError;
-  }
-  const [hashError, hashedPassword] = to(await bcrypt.hash(password, salt));
-  if (hashError) {
-    return hashError;
-  }
+  const salt = await bcrypt.genSalt(saltLength);
+  const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
 };
 
