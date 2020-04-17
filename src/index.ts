772a970f3798542dd296f5981a1cc258f6dfc91d
Max <montrealist@users.noreply.github.com>
2020-04-17 13:45:44 -0400

    added countElements function

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 export class Algorithms {
-    public static test(s: string = 'unknown person'): string {
-        return `Hello, ${s}`;
-    }
-    public static anotherTest(s: string = 'unknown person'): string {
-        return `Hello again, ${s}`;
+    /**
+     * @param {number[]} arr
+     * @return {number}
+     */
+    public static countElements(arr: number[]) {
+        const mySet = new Set();
+        for (let item of arr) {
+            mySet.add(item);
+        }
+        // console.log(mySet);
+        let counter: number = 0;
+        for (let item of arr) {
+            if (mySet.has(item + 1)) {
+                counter++;
+            }
+        }
+        // console.log('counter: ',counter);
+        return counter;
     }
 }
